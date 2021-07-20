c92c98a3d707a67921301de7051162335af941ff
Jose Ramos <vikingofdeath@yahoo.com>
2021-07-19 22:53:28 -0500

    Se aagrego la funcion de ver los contactos en el sidebar

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector("#menu-navbar");
 const overlay = document.querySelector(".overlay");
+const sidebarItems = [...document.querySelectorAll(".navbar__menu-item")];
+const navbarMenus = [...document.querySelector(".navbar").children];
+const backBtnContact = document.querySelector("#backContactMenu");
+
+const changeMenu = function () {
+	navbarMenus.forEach((item) => {
+		item.style.transform = `translateX(${this * -100}%)`;
+	});
+};
+
+sidebarItems.forEach((item) => {
+	if (item.dataset.menu === "contact") item.addEventListener("click", changeMenu.bind(1));
+});
+
+backBtnContact.addEventListener("click", changeMenu.bind(0));
 
 const modalChange = function () {
 	const move = String(this) === "remove" ? 0 : -100;
 	navbar.style.transform = `translateX(${move}%)`;
 	overlay.classList[this]("hidden");
 	navbar.classList[this]("hidden");
+	String(this) === "add" && changeMenu.call(0);
 };
 
 overlay.addEventListener("click", modalChange.bind("add"));
